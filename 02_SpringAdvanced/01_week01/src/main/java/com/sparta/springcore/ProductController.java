af0ca120756c673641ef5dbe01b738472cacec69
heejun <heejjuunn@gmail.com>
2021-09-30 17:20:48 +0900

    리팩토링-생성자 초기화 추가

--- 02_SpringAdvanced/01_week01/src/main/java/com/sparta/springcore/ProductController.java
+++ 02_SpringAdvanced/01_week01/src/main/java/com/sparta/springcore/ProductController.java
@@ -9,15 +9,17 @@ import java.util.List;
 @RequiredArgsConstructor // final로 선언된 멤버 변수를 자동으로 생성합니다.
 @RestController // JSON으로 데이터를 주고받음을 선언합니다.
 public class ProductController {
+    private final ProductService productService;
+    public ProductController(){
+        ProductService productService = new ProductService();
+        this.productService = productService;
+    }
 
     // 신규 상품 등록
     @PostMapping("/api/products")
     public Product createProduct(@RequestBody ProductRequestDto requestDto) throws SQLException {
 // 요청받은 DTO 로 DB에 저장할 객체 만들기
-        ProductService productService = new ProductService();
         Product product = productService.createProduct(requestDto);
-
-
 // 응답 보내기
         return product;
     }
@@ -25,7 +27,6 @@ public class ProductController {
     // 설정 가격 변경
     @PutMapping("/api/products/{id}")
     public Long updateProduct(@PathVariable Long id, @RequestBody ProductMypriceRequestDto requestDto) throws SQLException {
-        ProductService productService = new ProductService();
         Product product = productService.updateProduct(id,requestDto);
 // 응답 보내기
         return product.getId();
@@ -34,7 +35,6 @@ public class ProductController {
     // 등록된 전체 상품 목록 조회
     @GetMapping("/api/products")
     public List<Product> getProducts() throws SQLException {
-        ProductService productService = new ProductService();
         List<Product> products = productService.getProducts();
 
         return products;
--- 02_SpringAdvanced/01_week01/src/main/java/com/sparta/springcore/ProductService.java
+++ 02_SpringAdvanced/01_week01/src/main/java/com/sparta/springcore/ProductService.java
@@ -7,35 +7,34 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class ProductService {
+
+    private final ProductRepository productRepository;
+    public ProductService(){
+        ProductRepository productRepository = new ProductRepository();
+        this.productRepository = productRepository;
+    }
+
     public Product createProduct(ProductRequestDto requestDto) throws SQLException {
         Product product = new Product(requestDto);
-
-        ProductRepository productRepository = new ProductRepository();
         productRepository.createProduct(product);
 
         return product;
     }
 //////////////
     public Product updateProduct(Long id, ProductMypriceRequestDto requestDto) throws SQLException {
-        ProductRepository productRepository = new ProductRepository();
         Product product = productRepository.getProduct(id);
         if(product == null) {
             throw new NullPointerException("해당 아이디가 존재하지 않습니다.");
         }
-
         productRepository.updateMyprice(id,requestDto.getMyprice());
-
-
 // 응답 보내기 (업데이트된 상품 id)
         return product;
     }
 
 
     public List<Product> getProducts() throws SQLException {
-        ProductRepository productRepository = new ProductRepository();
         List<Product> products = productRepository.getProducts();
 
         return products;
-
     }
 }
