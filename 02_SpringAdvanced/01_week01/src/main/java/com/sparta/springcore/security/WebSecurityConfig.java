f10abf51ce56592883e97210139d1a52896bce7c
heejun <heejjuunn@gmail.com>
2021-10-01 11:14:04 +0900

    UserDetailsService,UserDetails구현, index.html 템플릿 엔진으로 수정

--- 02_SpringAdvanced/01_week01/src/main/java/com/sparta/springcore/security/WebSecurityConfig.java
+++ 02_SpringAdvanced/01_week01/src/main/java/com/sparta/springcore/security/WebSecurityConfig.java
@@ -8,45 +8,63 @@ import org.springframework.security.config.annotation.web.configuration.WebSecur
 import org.springframework.security.crypto.bcrypt.BCrypt;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.builders.WebSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+
 @Configuration
 @EnableWebSecurity // 스프링 Security 지원을 가능하게 함
 public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
 
     @Bean
-    public BCryptPasswordEncoder encodePassword(){
+    public BCryptPasswordEncoder encodePassword() {
         return new BCryptPasswordEncoder();
     }
 
     @Override
-    public void configure(WebSecurity web){
-        //h2-console사용에 대한 허용(CSRF, FrameOption 무시)...나중에 공부하고 일단 공식처럼 외우셈셈
-       web.ignoring().antMatchers("/h2-console/**");
+    public void configure(WebSecurity web) {
+// h2-console 사용에 대한 허용 (CSRF, FrameOptions 무시)
+        web
+                .ignoring()
+                .antMatchers("/h2-console/**");
     }
 
     @Override
     protected void configure(HttpSecurity http) throws Exception {
-        //회원관리처리 api(POST /user/**)에 대해 CSRF 무시
-        http.csrf().ignoringAntMatchers("/user/**");
+// 회원 관리 처리 API (POST /user/**) 에 대해 CSRF 무시
+        http.csrf()
+                .ignoringAntMatchers("/user/**");
 
         http.authorizeRequests()
 // image 폴더를 login 없이 허용
                 .antMatchers("/images/**").permitAll()
 // css 폴더를 login 없이 허용
                 .antMatchers("/css/**").permitAll()
-                //회원관리 관련처리는 모두 허용할거임.
+// 회원 관리 처리 API 전부를 login 없이 허용
                 .antMatchers("/user/**").permitAll()
-// 어떤 요청이든 '인증'
+// 그 외 어떤 요청이든 '인증'
                 .anyRequest().authenticated()
                 .and()
-// 로그인 기능 허용
-                    .formLogin()
-                    .loginPage("/user/login")
-                    .defaultSuccessUrl("/")
-                    .failureUrl("/user/login?error")
-                    .permitAll()
+// [로그인 기능]
+                .formLogin()
+// 로그인 View 제공 (GET /user/login) 이건 GET으로 올때 처리하는거고
+                .loginPage("/user/login")
+// 로그인 처리 (POST /user/login) 이건 Post로 올때 처리하는거. Spring Security의 Authentiction Manager가 작업하도록
+                .loginProcessingUrl("/user/login")
+// 로그인 처리 후 성공 시 URL
+                .defaultSuccessUrl("/")
+// 로그인 처리 후 실패 시 URL
+                .failureUrl("/user/login?error")
+                .permitAll()
                 .and()
-// 로그아웃 기능 허용
-                    .logout()
-                    .permitAll();
+// [로그아웃 기능]
+                .logout()
+// 로그아웃 처리 URL
+                .logoutUrl("/user/logout")
+                .permitAll();
     }
 }
